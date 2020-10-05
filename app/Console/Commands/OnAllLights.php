4f82f88b56b0fe260ed2e6a6400f32625760e719
dmb-dev87 <dmbdev800@gmail.com>
2020-10-05 10:09:55 -0600

    [TopOneExpert][2020-10-05][fix error]

--- app/Console/Commands/OnAllLights.php
+++ app/Console/Commands/OnAllLights.php
@@ -39,7 +39,7 @@ class OnAllLights extends Command
      */
     public function handle()
     {
-        $client = new Client(new Version1X('//localhost:8080/'));
+        $client = new Client(new Version1X('//172.31.29.200:8080/'));
 
         $client->initialize();
         
